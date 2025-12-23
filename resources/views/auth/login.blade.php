6bc4a829e37b9543a6e4e5fae31acf0a643fc42e
EmanuelSgx <emanuel.sg@proton.me>
2025-12-22 23:55:18 -0300

    Criando tela de login basica e rota

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <h1>Login</h1>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            <button type="submit">Login</button>
+        </div>
+</div>
